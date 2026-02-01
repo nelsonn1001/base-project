baf42463b3f79b585439bb994e4f6fd241802066
nelsonn1001 <nelsonn1001@gmail.com>
2026-02-01 07:43:30 -0500

    change param in package

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base:"/base-project",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    base:"/base-project",
 });
